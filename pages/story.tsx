a182675ff04ab75ac1648d835397ed7e61fc4157
Jeff Chheng <jeffchheng@gmail.com>
2020-01-26 19:33:15 -0800

    responsiveness

--- pages/story.tsx
+++ pages/story.tsx
@@ -30,7 +30,7 @@ export default function OurStory() {
                 <Link key={`img-${i}`} href={`/story/${i}`}>
                   <a>
                     <img
-                      className="inline-block object-cover p-4 w-full md:w-1/2 lg:w-1/3 h-64"
+                      className="inline-block object-cover p-4 w-full md:w-1/2 lg:w-1/3 h-full md:h-64"
                       src={`/story/${i}.jpg`} />
                   </a>
                 </Link>,
