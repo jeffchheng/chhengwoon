5ad7e5774d05e548f1a60ecfbc7c843a67c17d6d
Jeff Chheng <jeffchheng@gmail.com>
2020-01-26 16:36:00 -0800

    responsive design i s2g

--- components/PageHeaderSeparator.tsx
+++ components/PageHeaderSeparator.tsx
@@ -1,3 +1,3 @@
 export default function PageHeaderSeparator() {
-  return <div className="bg-purple-800 m-4 mx-auto h-1 min-h-full w-3/5 " />
+  return <div className="bg-purple-800 m-4 mx-auto h-1 min-h-full w-5/6 lg:w-3/5" />
 }
