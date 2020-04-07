901305070efa7a2cf0e649c309e1ace000fcf296
Jeff Chheng <jeffchheng@gmail.com>
2020-04-07 10:00:51 -0700

    max width instead of width

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -24,13 +24,14 @@ export default function RSVP() {
 
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSczb8Bz0NAQGvNWjzdewFbkfl7H6JTg0cakaWY7jn1Gfw9oVQ/viewform?embedded=true"
-          width="640"
           height="1280"
           frameBorder="0"
           style={{
             border: 0,
             display: "block",
             margin: "0 auto",
+            width: "100%",
+            maxWidth: 640,
           }}
         >
           Loading...
