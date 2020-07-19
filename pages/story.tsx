20dde185edea2ba0985e1f4a7072ae5bcc764baf
Jeff Chheng <jeffchheng@gmail.com>
2020-07-18 22:34:15 -0700

    Use grid instead of flexbox

--- pages/story.tsx
+++ pages/story.tsx
@@ -44,18 +44,12 @@ export default function OurStory() {
           Japan, Greece, Czech Republic, Switzerland), and are excited for our
           biggest adventure yet!
         </SectionText>
-        <div className="flex flex-wrap flex-row justify-center">
+        <div className="grid grid-cols-2 md:grid-cols-3 grid-flow-row gap-2">
           {(() => {
             const images = [];
 
             for (let i = 0; i <= 26; i++) {
-              images.push(
-                <img
-                  className="inline-block object-cover p-4 w-full h-full md:h-auto md:w-1/2 lg:w-1/3"
-                  height="600"
-                  src={`/story/${i}.jpg`}
-                />
-              );
+              images.push(<img src={`/story/${i}.jpg`} />);
             }
 
             return images;
